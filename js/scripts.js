6ff733ad2d531f9ec8f2856f2cae1b27d0805c26
dpatil1719 <patildhanashree888@gmail.com>
2025-04-02 15:38:11 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,15 @@ let pokemonList = [
   { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
   { name: "Venusaur", height: 2, types: ["grass", "poison"] },
 ];
+
+// Loop through each Pokémon in the list
+for (let i = 0; i < pokemonList.length; i++) {
+  //Store the output of name & height
+  let output = pokemonList[i].name + " (height: " + pokemonList[i].height + ")";
+
+  //Check if the height is more than 10
+  if (pokemonList[i].height > 6) output += " - Wow, that’s big!";
+
+  //Print output to webpage
+  document.write(output + "<br>");
+}
