1a9abc9c62a31471a1eafee7ec08672bddf476b5
dpatil1719 <patildhanashree888@gmail.com>
2025-04-12 12:47:54 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,49 @@
 // IIFE that contains the pokemonList
 let pokemonRepository = (function () {
     let pokemonList = [
-        { name: "Bulbasaur", height: 7, types: ["grass", "poison"] },
-        { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
-        { name: "Venusaur", height: 2, types: ["grass", "poison"] },
+        { name: "Bulbasaur", height: 7 },
+        { name: "Ivysaur", height: 1 },
+        { name: "Venusaur", height: 2 }
     ];
+
+    // Function to add a new Pokémon to the list
+    function add(pokemon) {
+        if (
+            typeof pokemon === "object" &&
+            pokemon !== null &&
+            Object.keys(pokemon).length === 2 &&
+            "name" in pokemon &&
+            "height" in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        } else {
+            console.error(" Invalid Pokémon. Must be an object with name and height.");
+        }
+    }
+
     // Function to return the list
     function getAll() {
         return pokemonList;
     }
-
-    // Function to add a new Pokémon to the list
-    function add(pokemon) {
-        pokemonList.push(pokemon);
+    // ✅ Filters Pokémon by name (case-insensitive)
+    function filterByName(name) {
+        return pokemonList.filter(function (pokemon) {
+            return pokemon.name.toLowerCase() === name.toLowerCase();
+        });
     }
-
     // Public API (returned object)
     return {
         getAll: getAll,
-        add: add
+        add: add,
+        filterByName: filterByName
     };
 })();
 
+
+
 //  Loop through each Pokémon using forEach
 pokemonRepository.getAll().forEach(function (pokemon) {
-    let description = pokemon.name + " (height: " + pokemon.height + ")" + " " +"(types:" + pokemon.types + ")";
+    let description = pokemon.name + " (height: " + pokemon.height + ")";
 
     if (pokemon.height > 6) {
         description += " - Wow, that’s big!";
@@ -32,4 +51,13 @@ pokemonRepository.getAll().forEach(function (pokemon) {
 
     document.write(description + "<br>");
 });
+
+//  Sample add & search test calls 
+pokemonRepository.add({ name: "Pikachu", height: 4 });
+pokemonRepository.add({ name: "Mewtwo", height: "tall" });
+pokemonRepository.add({ name: "Onix", height: 28 });
+
+console.log("All Pokémon:", pokemonRepository.getAll());
+console.log("Search 'Pikachu':", pokemonRepository.filterByName("Pikachu"));
+
 
